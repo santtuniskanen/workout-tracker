64178d3c564c8483c3582716914d46147a01889e
santtuniskanen <santtuniska02@gmail.com>
2024-02-14 14:18:48 +0200

    created the initial user model

---
+++ backend/models/userModel.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `gorm:"uniqueIndex;not null" json:"email"`
+	Password  string `gorm:"not null" json:"-"`
+}
+
+// Hashing passwords before saving
+func (u *User) BeforeSave(tx *gorm.DB) (err error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
